fa90932d888d9df6de80cd0efe255d7788bee2d6
nawelGl <ghazal.nawel@gmail.com>
2025-11-15 19:35:26 +0100

    core operational backend

---
+++ core-operational-backend/src/main/java/fr/upec/episen/core_operational_backend/cache/RegisteredPeopleCache.java
@@ -0,0 +1,18 @@
+package fr.upec.episen.core_operational_backend.cache;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisteredPeopleCache {
+    private static final String REGISTERED_SET = "set:registered_people";
+    private final StringRedisTemplate redis;
+
+    public RegisteredPeopleCache(StringRedisTemplate redis) {
+        this.redis = redis;
+    }
+
+    public boolean isRegistered(String badgeId) {
+        return Boolean.TRUE.equals(redis.opsForSet().isMember(REGISTERED_SET, badgeId));
+    }
+}
---
+++ core-operational-backend/src/test/java/fr/upec/episen/core_operational_backend/CoreOperationalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.core_operational_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreOperationalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
